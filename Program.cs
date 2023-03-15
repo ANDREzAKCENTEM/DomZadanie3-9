a979a5e2ce44fc600eb0c866e9b6e59e6c7fc7a9
Andy-Koo <DarikRunech@gmail.com>
2023-03-15 12:10:03 +0300

    ReshenijaFinal

--- Program.cs
+++ Program.cs
@@ -25,6 +25,39 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// Console.WriteLine("Введите число M: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+// NaturalNumbers(numberM, numberN);
+// int NaturalNumbers(int numM, int numN)
+// {
+//     if (numM > numN) return numN + NaturalNumbers(numN + 1, numM);
+//     return numM == numN ? numM : numM + NaturalNumbers(numM + 1, numN);
+// }
+
+// int numbersNatural = NaturalNumbers(numberM, numberN);
+// Console.WriteLine(numbersNatural);
+
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+// Console.WriteLine("Вычисление функции Аккермана");
+// Console.WriteLine("Желательно вводить одно из значений от 0 до 3, иначе расчет уходит в бесконечность!!!");
+// Console.WriteLine("Введите по очереди два числа m и n: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(numberM, numberN);
+// int AkkermanFunction(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return AkkermanFunction(m - 1, 1);
+//     else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+// }
+
+// Console.Write("-> A(m,n) = ");
+// int functionAkkerman = AkkermanFunction(numberM, numberN);
+// Console.WriteLine(functionAkkerman);
